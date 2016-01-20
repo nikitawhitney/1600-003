fc028283be42e80ee23b37fa3910df8ece4f0395
Niki Burton <nikiinslc@yahoo.com>
2016-01-20 09:19:13 -0700

    no message

---
+++ New Unity Project/Assets/VarandFunc.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VarandFunc : MonoBehaviour
+{
+    int myInt = 50;
+    // Use this for initialization
+    void Start() {
+
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+    int MultiplyByTwo (int number)
+    { 
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
