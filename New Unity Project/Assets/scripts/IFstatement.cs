e186562770fd3a7fc8dbfd2e8e67106377d0466f
Niki Burton <nikiinslc@yahoo.com>
2016-02-05 09:53:29 -0700

    Ifstatement redo

--- New Unity Project/Assets/scripts/IFstatement.cs
+++ New Unity Project/Assets/scripts/IFstatement.cs
@@ -1,42 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IFstatement : MonoBehaviour {
+public class IFStatement : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
 
-    // Use this for initialization
-    void Start()
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
-        float coffeeTemperature = 85.0f;
-        float hotLimitTemperature = 70.0f;
-        float coldLimitTemperature = 40.0f;
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
 
+        coffeeTemperature -= Time.deltaTime * 5f;
+        	
+	}
 
-        
-        // Update is called once per frame
-        void Update ()
+    void TemperatureTest()
     {
-            if (Input.GetKeyDown(KeyCode.Space))
-                TemperatureTest();
-
-            coffeeTemperature -= Time.deltaTime * 5f;
-
-    }
-
-        void TemperatureTest ()
-        // if the coffee's temperature is greater than the hottest drinking temperature...
-        if (coffeeTemperature > hotLimitTemperature)
+        // If the coffee's temperature is greater than the hottest drinking temp..
+        if(coffeeTemperature > hotLimitTemperature)
         {
             print("Coffee is too hot.");
         }
 
-        // if it isn't , but the coffee temperature is less than the coldest drinking temperature...
-        else if (coffeeTemperature < coldLimitTemperature)
+        //If it isn't, but the coffee temp is less than coldest drinking temp...
+        else if(coffeeTemperature < coldLimitTemperature)
         {
             print("Coffee is too cold.");
         }
 
-        //if it is neither of those then..
-        else { print("Coffee is just right."); }
-     }
-    }
+        //If it is neither of those then...
+        else { print("Coffee is just right.");
+        }
 
+    }
+}
