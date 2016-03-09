d2e17cdf64ad2131cbd77740acad5cb87944a214
Niki Burton <nikiinslc@yahoo.com>
2016-03-09 09:54:25 -0700

    IF update

--- New Unity Project/Assets/scripts/IFstatement.cs
+++ New Unity Project/Assets/scripts/IFstatement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class IFStatement : MonoBehaviour
 {
-    public float coffeeTemperature = 50.0f;
+    public float hotchocolateTemperature = 50.0f;
     public float hotLimitTemperature = 35.0f;
     public float coldLimitTemperature = 25.0f;
 
@@ -19,27 +19,27 @@ public class IFStatement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             TemperatureTest();
 
-        coffeeTemperature -= Time.deltaTime * 5f;
+        hotchocolateTemperature -= Time.deltaTime * 5f;
 
     }
 
     void TemperatureTest()
     {
         // If the coffee's temperature is greater than the hottest drinking temp..
-        if (coffeeTemperature > hotLimitTemperature)
+        if (hotchocolateTemperature > hotLimitTemperature)
         {
-            print("Coffee is too hot.");
+            print("Hot Chocolate is too hot.");
         }
 
         //If it isn't, but the coffee temp is less than coldest drinking temp...
-        else if (coffeeTemperature < coldLimitTemperature)
+        else if (hotchocolateTemperature < coldLimitTemperature)
         {
-            print("Coffee is too cold.");
+            print("Hot Chocolate is too cold.");
         }
 
         //If it is neither of those then...
         else {
-            print("Coffee is just right.");
+            print("Hot Chocolate is just right.");
         }
 
     }
