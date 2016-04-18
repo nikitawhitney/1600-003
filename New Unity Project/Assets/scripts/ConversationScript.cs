750552751ea509a6f36fb36c199af9f36bb3d13c
Niki Burton <nikiinslc@yahoo.com>
2016-04-18 00:23:42 -0600

    switch statement

---
+++ New Unity Project/Assets/scripts/ConversationScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationScript : MonoBehaviour
+{
+
+    public int intelligence = 4;
+
+        void Greet ()
+    {
+        switch (intelligence)
+        {
+            case 4:
+                print("What a well behaved and lovely dog you have!");
+                break;
+            case 3:
+                print("Wow! What a cute dog! ");
+                break;
+            case 2:
+                print("OMG a dog!");
+                break;
+            case 1:
+                print("!!!!!! DOG!! so smol and fluf!!");
+                break;
+            default:
+                print("Incorrect intelligence level.");
+                break;
+        }
+    }
+	
+}
