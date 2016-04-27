e52ff4f7a8f707d394d3995854ea940f379a5aa0
Niki Burton <nikiinslc@yahoo.com>
2016-04-27 05:04:17 -0600

    done

---
+++ Game Final/Assets/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+	// Use this for initialization
+	void Awake ()
+    {
+        anim = GetComponent<Animator> ();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(playerHealth.currentHealth<=0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if(restartTimer>= restartDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+}
