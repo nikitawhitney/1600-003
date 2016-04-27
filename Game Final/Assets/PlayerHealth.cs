eaaa4533a48a3abcfcfede27a52cac33cd377b21
Niki Burton <nikiinslc@yahoo.com>
2016-04-26 23:23:19 -0600

    Player health script

---
+++ Game Final/Assets/PlayerHealth.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+    Animator anim;
+    PlayerMovement playerMovement;
+    //PlayerShooting playerShooting;
+    bool isDead;
+    bool damaged;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        //playerShooting = GetComponentInChildren <PlayerShooting>();
+        currentHealth = startingHealth;
+    }
+	
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(damaged)
+        {
+            damageImage.color = flashColor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+	
+	}
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death ()
+    {
+        isDead = true;
+
+        //playerShooting.DisableEffects ();
+        anim.SetTrigger("Die");
+
+        playerMovement.enabled = false;
+        //playerShooting.enabled = false;
+    }
+}
