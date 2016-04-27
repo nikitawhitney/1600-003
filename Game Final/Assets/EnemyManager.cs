9e107c383cb26a02cb253b105a06c9153965d73c
Niki Burton <nikiinslc@yahoo.com>
2016-04-27 02:24:28 -0600

    environment and enemies

---
+++ Game Final/Assets/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
+       	
+	}
+	
+	// Update is called once per frame
+	void Spawn ()
+    {
+	    if(playerHealth.currentHealth <=0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
